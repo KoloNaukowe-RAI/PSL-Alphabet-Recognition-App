e498bafaeb01b68c667d42cc79d94651bf597ec5
mmcza <marcin4c.mc@gmail.com>
2024-06-29 23:41:28 +0200

    Updated requirements.txt, added template for data_augmentation (TODO)

---
+++ model/data_augmentation.py
@@ -0,0 +1,13 @@
+import numpy as np
+import os
+
+def data_augmentation(path_to_file):
+    # Load the data
+    data = np.load(path_to_file)
+    # Augment the data
+    ### TO DO ###
+    augmented_data = data
+
+
+    # Save the augmented data
+    return augmented_data
