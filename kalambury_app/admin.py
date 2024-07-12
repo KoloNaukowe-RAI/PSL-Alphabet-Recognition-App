fc7b99b327563cd9dbc02ac69b9dc3a014dfe7f2
Daria Kubacka <126802160+dariak153@users.noreply.github.com>
2024-07-12 17:41:20 +0200

    Update admin.py

--- kalambury_app/admin.py
+++ kalambury_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Feedback
 
-# Register your models here.
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'gender', 'ratingBefore', 'ratingAfter', 'message')
+
+admin.site.register(Feedback, FeedbackAdmin)
