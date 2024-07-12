c37cae2d4802f8d986782a6b6e189ab0e4d725cc
Daria Kubacka <126802160+dariak153@users.noreply.github.com>
2024-07-12 17:42:53 +0200

    Update urls.py

--- kalambury_app/urls.py
+++ kalambury_app/urls.py
@@ -1,18 +1,18 @@
-
-
 from django.urls import path
-from .views import HomeView, StartGameView, ProcessVideoFrameView, LiveCameraFeedView,  SignsView, QRCodeView
+from .views import HomeView, StartGameView, ProcessVideoFrameView, LiveCameraFeedView, SignsView, QRCodeView
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
+from .views import feedback_view, feedback_thanks_view
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('signs/', SignsView.as_view(), name='signs'),
-    path('start-game/', StartGameView.as_view(), name='start-game'),
     path('qr_code/', QRCodeView.as_view(), name='qr_code'),
+    path('start-game/', StartGameView.as_view(), name='start-game'),
     path('process-frame/', ProcessVideoFrameView.as_view(), name='process-frame'),
     path('live-camera-feed/', LiveCameraFeedView.as_view(), name='live_camera_feed'),
+    path('feedback/', feedback_view, name='feedback'),
+    path('feedback/thanks/', feedback_thanks_view, name='feedback_thanks'),
 ]
 
 if settings.DEBUG:
