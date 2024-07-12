b7008322c8db70912212f26f3a130f8e4139dc6a
Daria Kubacka <126802160+dariak153@users.noreply.github.com>
2024-07-12 17:40:57 +0200

    Update models.py

--- kalambury_app/models.py
+++ kalambury_app/models.py
@@ -1,2 +1,8 @@
-from django.db import models
+from django import forms
+from .models import Feedback
+
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model = Feedback
+        fields = ['name', 'email', 'gender', 'ratingBefore', 'ratingAfter', 'message']
 
