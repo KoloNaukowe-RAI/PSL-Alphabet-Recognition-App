8e932590b6e7b34517b54faa424c152028ab9d3e
mmcza <marcin4c.mc@gmail.com>
2024-07-16 22:04:11 +0200

    Update for data augmentation

--- model/data_augmentation.py
+++ model/data_augmentation.py
@@ -43,21 +43,33 @@ def rotate_point(point, origin, rotation):
 
     return rotated_point
 
-def data_augmentation(path_to_data, path_to_labels, number_of_augmented_samples=10, min_rotation=-5, max_rotation=5, min_scale=0.9, max_scale=1.1):
+def data_augmentation(path_to_dir, number_of_augmented_samples=10, min_rotation=-5, max_rotation=5, min_scale=0.9, max_scale=1.1):
     # Load the data
-    data = np.load(path_to_data)
-    labels = np.load(path_to_labels)
+    data = np.load(os.path.join(path_to_dir, "recorded_data.npy"))
+    labels = np.load(os.path.join(path_to_dir, "labels.npy"))
+    data_long = np.load(os.path.join(path_to_dir, "recorded_data_long.npy"))
+    labels_long = np.load(os.path.join(path_to_dir, "labels_long.npy"))
+
+    # Check if the data has already been augmented and if so, delete it
+    if os.path.exists(os.path.join(path_to_dir, 'augmented_data.npy')):
+        os.remove(os.path.join(path_to_dir, 'augmented_data.npy'))
+        os.remove(os.path.join(path_to_dir, 'augmented_labels.npy'))
 
     # Augment the data
     augmented_data = []
     augmented_labels = []
-    number_of_samples = data.shape[0]
+    number_of_samples = data.shape[0] + data_long.shape[0]
+    data_len = data.shape[0]
     for i in range(number_of_samples):
         print("Currently augmenting sample: ", i + 1, "/", number_of_samples)
 
         # Add the original data
-        augmented_data.append(data[i])
-        augmented_labels.append(labels[i])
+        if i < data_len:
+            augmented_data.append(np.repeat(data[i], 2, axis=0))
+            augmented_labels.append(labels[i])
+        else:
+            augmented_data.append(data_long[i - data_len])
+            augmented_labels.append(labels_long[i - data_len])
 
         # Generate random rotation and scale
         rotation = np.random.uniform(min_rotation, max_rotation, (number_of_augmented_samples, 3))
@@ -65,7 +77,10 @@ def data_augmentation(path_to_data, path_to_labels, number_of_augmented_samples=
 
         # Iterate over the number of augmented samples and augment the data
         for j in range(number_of_augmented_samples):
-            data_to_augment = np.copy(data[i])
+            if i < data_len:
+                data_to_augment = np.copy(data[i])
+            else:
+                data_to_augment = np.copy(data_long[i - data_len])
 
             # Iterate over each frame
             for k in range(data_to_augment.shape[0]):
@@ -93,9 +108,32 @@ def data_augmentation(path_to_data, path_to_labels, number_of_augmented_samples=
             data_to_augment[:, :, 1] += translation_y
             data_to_augment[:, :, 2] += translation_z
 
+            if i < data_len:
+                # Duplicate each frame
+                data_to_augment = np.repeat(data_to_augment, 2, axis=0)
+
             # Append the augmented data
             augmented_data.append(data_to_augment)
             augmented_labels.append(labels[i])
+
+        if i % 100 == 1 or i == (number_of_samples - 1) and i != 0:
+            # Save the augmented data to the disk to prevent memory issues and make it run faster
+            augmented_data_np = np.array(augmented_data)
+            augmented_labels_np = np.array(augmented_labels)
+
+            if os.path.exists(os.path.join(path_to_dir, 'augmented_data.npy')):
+                all_data = np.load(os.path.join(path_to_dir, 'augmented_data.npy'))
+                all_labels = np.load(os.path.join(path_to_dir, 'augmented_labels.npy'))
+                all_data = np.concatenate((all_data, augmented_data_np), axis=0)
+                all_labels = np.concatenate((all_labels, augmented_labels_np), axis=0)
+                np.save(os.path.join(path_to_dir, 'augmented_data.npy'), all_data)
+                np.save(os.path.join(path_to_dir, 'augmented_labels.npy'), all_labels)
+            else:
+                np.save(os.path.join(path_to_dir, 'augmented_data.npy'), augmented_data_np)
+                np.save(os.path.join(path_to_dir, 'augmented_labels.npy'), augmented_labels_np)
+            augmented_data = []
+            augmented_labels = []
+
 
     # Convert lists to numpy arrays
     augmented_data = np.array(augmented_data)
@@ -119,15 +157,13 @@ def save_to_firebase(augmented_data, augmented_labels):
 if __name__ == '__main__':
 
     #ADD additional '..' if directories dont match or just navigate to designated folder with PL_Sign_Language
-    path_to_data = os.path.join( '..', 'PL_Sign_Language_Letters_Recognition', 'data_for_models', '24_07_01_17_41_19', 'recorded_data.npy')
-    path_to_labels = os.path.join( '..', 'PL_Sign_Language_Letters_Recognition', 'data_for_models', '24_07_01_17_41_19',
-                                'labels.npy')
-    augmented_data, augmented_labels = data_augmentation(path_to_data, path_to_labels)
+    dir_with_data = os.path.join( '..', '..', 'PL_Sign_Language_Letters_Recognition', 'data_for_models', '24_07_16_20_58_47')
+    augmented_data, augmented_labels = data_augmentation(dir_with_data)
     print(augmented_data.shape)
 
-    path_to_save_data = os.path.join( '..', 'PL_Sign_Language_Letters_Recognition', 'data_for_models', '24_07_01_17_41_19', 'augmented_data.npy')
-    path_to_save_labels = os.path.join( '..', 'PL_Sign_Language_Letters_Recognition', 'data_for_models', '24_07_01_17_41_19', 'augmented_labels.npy')
-    np.save(path_to_save_data, augmented_data)
-    np.save(path_to_save_labels, augmented_labels)
+    # path_to_save_data = os.path.join(dir_with_data, 'augmented_data.npy')
+    # path_to_save_labels = os.path.join(dir_with_data, 'augmented_labels.npy')
+    # np.save(path_to_save_data, augmented_data)
+    # np.save(path_to_save_labels, augmented_labels)
 
     #save_to_firebase(augmented_data, augmented_labels)
